ca544d806511c76d1808e2e3b978aa007d0fcd71
ejohnson24 <ejohnson24@users.noreply.github.com>
2020-03-09 11:58:34 -0700

    changes to background images for cast and other edits

--- js/scripts.js
+++ js/scripts.js
@@ -17,24 +17,49 @@ document.addEventListener('DOMContentLoaded', (event) => {
     let body = document.querySelector('body');
     let header = document.querySelector('header');
     let info = document.querySelector('.info');
-
+    let footer = document.querySelector('footer')
      button.addEventListener( 'click' , function(){
     
      cat.classList.toggle('catDark')
      info.classList.toggle('darkInfo');
      header.classList.toggle('darkHeader');
      body.classList.toggle('dark');
+     footer.classList.toggle('darkfooter')
 });
 
-    let portrait = document.querySelector('#tombo');
-    let background = document.querySelector('.cast');
+let portrait = document.querySelector('#tombo');
+let background = document.querySelector('.cast');
+let portrait2 = document.querySelector('#Osono');
+let portrait3 = document.querySelector('#Ursula');
+let portrait4 = document.querySelector('#Kiki');
+let portrait5 = document.querySelector('#Jiji');
 
-    portrait.addEventListener( 'click' , function(){
-    
-      background.classList.toggle('castTombo');
- });
+portrait.addEventListener( 'click' , function(){
+
+  background.classList.toggle('castTombo');
 
 });
 
+portrait3.addEventListener( 'click', function() {
+
+  background.classList.toggle('castUrsula');
+});
+
+portrait2.addEventListener( 'click', function() {
+
+  background.classList.toggle('castOsono');
+});
+
+portrait4.addEventListener( 'click', function() {
+
+  background.classList.toggle('castKiki');
+});
+
+portrait5.addEventListener( 'click', function() {
+
+  background.classList.toggle('castJiji');
+});
+
+});
 
 
